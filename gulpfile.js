fb73f1d2a3d507457a239829e347c500bc046a18
Stefan Zamfira <stefan.zamfira@gmail.com>
2017-01-18 19:26:52 +0200

    basic front-end automatization tasks

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+var PATHS = {
+    SASS: {
+        SRC: 'scss/**/*.scss',
+        DIST: 'css'
+    }
+}
+
+
+gulp.task('sass-dev', function(){
+    var opts = {
+
+    };
+
+    return gulp.src(PATHS.SASS.SRC)
+            .pipe(sass(opts))
+            .pipe(gulp.dest(PATHS.SASS.DIST));
+});
+
+
+
+gulp.task('watch', function(){
+    gulp.watch(PATHS.SASS.SRC, ['sass-dev']);
+})
